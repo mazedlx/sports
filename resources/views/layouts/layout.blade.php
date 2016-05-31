b7eb07e5f53664568eb5e6e51ff64389b8cc3d69
Christian Leo <mazedlx@gmail.com>
2016-05-31 23:36:35 +0200

    CSS and JS

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,8 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>sports.mazedlx.net</title>
-        <link rel="stylesheet" href="https://lego.mazedlx.net/css/bootstrap.css">
-        <script src="https://lego.mazedlx.net/js/all.js"></script>
+        <link rel="stylesheet" href="/css/app.css">
+        <script src="js/app.js"></script>
     </head>
     <body>
         <div class="container">
