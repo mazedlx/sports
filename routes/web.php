49ca9c34f5375fe90e5c4e32ac0d4f9b69f6c364
Christian Leo-Pernold <mazedlx@gmail.com>
2019-01-01 18:44:54 +0100

    Fix routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,26 @@
 <?php
 
-Route::get('/', 'PoolController@index');
-Route::get('/sports', 'PoolController@index');
-Route::get('/pool/{year}/{locaton_id}', 'PoolController@show');
-Route::get('/sports/pool/{year}/{locaton_id}', 'PoolController@show');
+Auth::routes(['register' => false]);
 
-Route::get('/create/day', 'PoolController@createDay');
-Route::get('/create/results/{day_id}/{location_id}', 'PoolController@createResults');
-Route::get('/create/games/{day_id}/{location_id}', 'PoolController@createGames');
 
-Route::post('/store/day', 'PoolController@storeDay');
-Route::post('/store/results', 'PoolController@storeResults');
-Route::post('/store/games', 'PoolController@storeGames');
+Route::get('/', 'PageController');
+Route::get('/sports', 'PageController');
+
+Route::get('/pool/{year}/{location}', 'PoolController@show');
+Route::get('/sports/pool/{year}/{location}', 'PoolController@show');
 
 Route::get('/game/{id}', 'GameController@show');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/create/day', 'PoolController@createDay');
+    Route::get('/create/results/{day}/{location}', 'PoolController@createResults');
+    Route::get('/create/games/{day}/{location}', 'PoolController@createGames');
+
+    Route::post('/store/day', 'PoolController@storeDay');
+    Route::post('/store/results', 'PoolController@storeResults');
+    Route::post('/store/games', 'PoolController@storeGames');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
