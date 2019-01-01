652e776ee284d4d163471b8a30327f2f41e3847c
Christian Leo-Pernold <mazedlx@gmail.com>
2019-01-01 18:45:13 +0100

    Refactoring

--- app/Team.php
+++ app/Team.php
@@ -13,12 +13,12 @@ class Team extends Model
         return $this->hasMany('App\Game');
     }
 
-    public function scopeHasLocation($query, $location_id)
+    public function scopeHasLocation($query, Location $location)
     {
         return $query
             ->select('pool_team.*')
             ->join('pool_team_location', 'pool_team_location.id_team', '=', 'pool_team.id')
-            ->where('pool_team_location.id_location', '=', $location_id)
+            ->where('pool_team_location.id_location', '=', $location->id)
             ->orderBy('pool_team.team', 'asc');
     }
 }
