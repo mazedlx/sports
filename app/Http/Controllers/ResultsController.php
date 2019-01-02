2b84d84ef75e56cd6d054bda61827982025028c8
Christian Leo-Pernold <mazedlx@gmail.com>
2019-01-02 09:22:17 +0100

    Add missing methods

--- app/Http/Controllers/ResultsController.php
+++ app/Http/Controllers/ResultsController.php
@@ -10,6 +10,93 @@ use Illuminate\Http\Request;
 
 class ResultsController extends Controller
 {
+    public function show($year, Location $location)
+    {
+        $scoresAvg = getRanking(
+            Player::average($location, $year)->mapWithKeys(function ($player) {
+                return [$player->name => $player->score_avg];
+            })->toArray(),
+            'down'
+        );
+
+        $scoresTotal = getRanking(
+            Player::total($location, $year)->mapWithKeys(function ($player) {
+                return [$player->name => $player->score_total];
+            })->toArray(),
+            'down'
+        );
+
+        $maxPluses = getRanking(
+            Player::maxPlus($location, $year)->mapWithKeys(function ($player) {
+                return [$player->name => $player->max_plus];
+            })->toArray(),
+            'down'
+        );
+
+        $maxMinuses = getRanking(
+            Player::maxMinus($location, $year)->mapWithKeys(function ($player) {
+                return [$player->name => $player->max_minus];
+            })->toArray(),
+            'up'
+        );
+
+        $avgPluses = getRanking(
+            Player::avgPlus($location, $year)->mapWithKeys(function ($player) {
+                return [$player->name => $player->avg_plus];
+            })->toArray(),
+            'down'
+        );
+
+        $avgMinuses = getRanking(
+            Player::avgMinus($location, $year)->mapWithKeys(function ($player) {
+                return [$player->name => $player->avg_minus];
+            })->toArray(),
+            'up'
+        );
+
+        $payers = Player::payer($location, $year);
+        $last_payers = Player::lastPayer($location, $year);
+        $absentees = Player::absent($location, $year);
+        $performances = Player::performance($location, $year, false);
+
+        $scores = Player::performance($location, $year, true);
+        $i = 1;
+        $j = count($scores);
+        foreach ($scores as $team => $score) {
+            if ($i === 1) {
+                $scores[$team] = '<span class="first">' . $score . '</span>';
+            }
+            if ($i === 2) {
+                $scores[$team] = '<span class="second">' . $score . '</span>';
+            }
+            if ($i == $j) {
+                $scores[$team] = '<span class="last">' . $score . '</span>';
+            }
+            if ($i == $j - 1) {
+                $scores[$team] = '<span class="penultimate">' . $score . '</span>';
+            }
+            $i++;
+        }
+        return view('pool.results', [
+            'absentees' => $absentees,
+            'avg_minuses' => $avgMinuses,
+            'avg_pluses' => $avgPluses,
+            'last_payers' => $last_payers,
+            'location' => $location,
+            'max_minuses' => $maxMinuses,
+            'max_pluses' => $maxPluses,
+            'payers' => $payers,
+            'performances' => $performances,
+            'players' => $location->players()->sortByName()->get(),
+            'results' => Result::hasLocation($location)->year($year)->sortByDate()->get(),
+            'scores_avg' => $scoresAvg,
+            'scores_total' => $scoresTotal,
+            'scores' => $scores,
+            'totalFrames' => $location->totalFrames($year),
+            'year' => $year,
+        ]);
+    }
+
     public function create(Day $day, Location $location)
     {
         return view('pool.createresults', [
