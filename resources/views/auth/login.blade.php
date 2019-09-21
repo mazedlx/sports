6a67b4a6fa1f8f41596b6ee0aae2f15d3d6d41c3
Christian Leo-Pernold <mazedlx@gmail.com>
2019-09-21 22:02:15 +0200

    Fix login form.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,49 +1,41 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="card-header">{{ __('Login') }}</div>
+<div class="flex w-1/2 mx-auto flex-col">
+    <div class="text-2xl pb-2">{{ __('Login') }}</div>
 
-<div class="card-body">
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+    <div class="flex">
+        <form method="POST" action="{{ route('login') }}" class="flex flex-col border-red-500">
+            @csrf
 
-        <div class="form-group row">
-            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+            <div class="flex pb-2">
+                <div class="">
+                    <input id="email" type="email" class="px-4 py-2 border rounded {{ $errors->has('email') ? ' border-red-500' : '' }}" name="email" value="{{ old('email') }}" required autofocus placeholder="email">
 
-            <div class="col-md-6">
-                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                @if ($errors->has('email'))
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $errors->first('email') }}</strong>
-                    </span>
-                @endif
+                    @if ($errors->has('email'))
+                        <span class="" role="alert">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                </div>
             </div>
-        </div>
 
-        <div class="form-group row">
-            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+            <div class="flex pb-2">
+                <div class="">
+                    <input id="password" type="password" class="px-4 py-2 border rounded {{ $errors->has('password') ? ' border-red-500' : '' }}" name="password" required placeholder="password">
 
-            <div class="col-md-6">
-                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                @if ($errors->has('password'))
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                @endif
+                    @if ($errors->has('password'))
+                        <span class="" role="alert">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </div>
             </div>
-        </div>
 
-
-
-        <div class="form-group row mb-0">
-            <div class="col-md-8 offset-md-4">
-                <button type="submit" class="btn btn-primary">
-                    {{ __('Login') }}
-                </button>
-            </div>
-        </div>
-    </form>
+            <button type="submit" class="border rounded px-4 py-2 hover:bg-green-dark hover:text-white hover:border-green-dark">
+                {{ __('Login') }}
+            </button>
+        </form>
+    </div>
 </div>
 @endsection
