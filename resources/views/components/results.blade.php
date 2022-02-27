8845be4a46e2699994f576bfc50654a880ead2d8
Christian Leo-Pernold <christian.leo@akwien.at>
2022-02-27 13:30:50 +0100

    WIP.

--- resources/views/components/results.blade.php
+++ resources/views/components/results.blade.php
@@ -102,24 +102,42 @@
                 @click.away="showModal = false"
             >
                 <h1 class="text-3xl font-bold" x-text="`Games für ${date}`"></h1>
-                <table>
-                    <thead>
-                        <tr>
-                            <th >Frame #</th>
-                            <th >Gewinner</th>
-                            <th >Verlierer</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <template x-for="game in games">
-                            <tr>
-                                <td x-text="game.game_no + 1"></td>
-                                <td x-text="game.winner[0].team.team"></td>
-                                <td x-text="game.loser[0].team.team"></td>
-                            </tr>
-                        </template>
-                    </tbody>
-                </table>
+                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                        <div class="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
+                            <table class="min-w-full divide-y divide-gray-200">
+                                <thead class="bg-gray-50">
+                                    <tr>
+                                        <th
+                                            scope="col"
+                                            class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                                        >Frame #</th>
+                                        <th
+                                            scope="col"
+                                            class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                                        >Gewinner</th>
+                                        <th
+                                            scope="col"
+                                            class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                                        >Verlierer</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <template x-for="(game, index) in games">
+                                        <tr x-bind:class="{
+                                            'bg-white': index % 2 === 0,
+                                            'bg-gray-50': index % 2 !== 0,
+                                        }">
+                                            <td class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap" x-text="game.game_no + 1"></td>
+                                            <td class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap" x-text="game.winner[0].team.team"></td>
+                                            <td class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap" x-text="game.loser[0].team.team"></td>
+                                        </tr>
+                                    </template>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
