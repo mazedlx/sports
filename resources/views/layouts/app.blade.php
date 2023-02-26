a7796138200aa8cd1f506cfbb1cdb60141fde344
Christian Leo-Pernold <christian.leo@akwien.at>
2023-02-26 16:20:29 +0100

    WIP.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,31 +1,42 @@
 <!DOCTYPE html>
-<html lang="de" class="h-full">
+<html
+    lang="de"
+    class="h-full"
+>
+
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta
+        http-equiv="X-UA-Compatible"
+        content="IE=edge"
+    >
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+    >
     <title>{{ config('app.name') }}</title>
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <script src="{{ mix('js/app.js') }}" defer></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="h-full">
-        <div class="min-h-full">
-            <x-nav />
 
-            <div class="py-10">
-                <header>
-                    <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-                        <h1 class="text-3xl font-bold leading-tight text-gray-900">
-                            @yield('heading')
-                        </h1>
-                    </div>
-                </header>
-                <main>
-                    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                        @yield('content')
-                    </div>
-                </main>
-            </div>
+<body class="h-full">
+    <div class="min-h-full">
+        <x-nav />
+
+        <div class="py-10">
+            <header>
+                <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <h1 class="text-3xl font-bold leading-tight text-gray-900">
+                        @yield('heading')
+                    </h1>
+                </div>
+            </header>
+            <main>
+                <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    @yield('content')
+                </div>
+            </main>
         </div>
-    </body>
-    </html>
+    </div>
+</body>
+
+</html>
