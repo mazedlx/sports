2af22375069d5d881626c091ef65a6f8a0fe8fef
Christian Leo-Pernold <christian.leo@akwien.at>
2023-02-26 16:20:53 +0100

    WIP.

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -3,10 +3,9 @@
 @task('deploy', ['on' => ['web']])
     cd /var/www/html/sports.mazedlx.net
     git stash
-    php8.2 artisan down
+    php artisan down
     git pull origin main
     composer install --no-dev
-    php8.2 artisan optimize
-    php8.2 artisan queue:restart
-    php8.2 artisan up
+    php artisan optimize
+    php artisan up
 @endtask
